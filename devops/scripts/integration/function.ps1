242b38d6fad2314e07f4144fba7ccfd7cdda5b51
ryanpfalz <19982322+ryanpfalz@users.noreply.github.com>
2023-01-12 21:25:05 -0600

    iac

--- devops/scripts/integration/function.ps1
+++ devops/scripts/integration/function.ps1
@@ -7,7 +7,7 @@ $envConfig = $config.$($config.env)
 
 $rgName = $envConfig.resourceGroup
 $location = $envConfig.location
-$broadcastFunctionName = $envConfig.broadcastFunctionName
+$apiFunctionName = $envConfig.apiFunctionName
 $storageAccount = $envConfig.storageAccountName
 $appServicePlan = $envConfig.appServicePlan
 $pythonVersion = $envConfig.functionPythonVersion
@@ -28,7 +28,7 @@ az storage account create --name $storageAccount --resource-group $rgName --sku 
 # TODO: update plan used by function
 # note: automatically creates app insights resource
 # broadcast & negotiate will be deployed to the same resource
-az functionapp create --name $broadcastFunctionName --resource-group $rgName --storage-account $storageAccount --os-type "Linux"  --runtime "python" --runtime-version $pythonVersion --functions-version 4 --plan $appServicePlan # --consumption-plan-location "centralUS"
+az functionapp create --name $apiFunctionName --resource-group $rgName --storage-account $storageAccount --os-type "Linux"  --runtime "python" --runtime-version $pythonVersion --functions-version 4 --plan $appServicePlan # --consumption-plan-location "centralUS"
 
 Write-Host "Created Storage Account and Function"
 
